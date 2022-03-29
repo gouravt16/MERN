c9ee0f29ee779b947d3f635f8250aee6387b8c89
gouravt16 <49191244+gouravt16@users.noreply.github.com>
2022-03-30 03:18:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
 import Homepage from "./components/Homepage";
 import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { HomepageData } from "./data/HomepageData";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <HomepageData>
+      <Header />
       <Router>
         <Routes>
           <Route exact path="/" element={<Homepage />}></Route>
           <Route exact path="/user/:id" element={<UserProfile />}></Route>
         </Routes>
       </Router>
+      <Footer />
     </HomepageData>
   );
 }
