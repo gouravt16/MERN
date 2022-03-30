70535f6ace805698eed683350186a6d95aed22ab
gouravt16 <49191244+gouravt16@users.noreply.github.com>
2022-03-30 15:06:18 +0530

    Add files via upload

--- src/data/HomepageData.js
+++ src/data/HomepageData.js
@@ -1,7 +1,6 @@
 import { useState, useEffect, createContext } from "react";
 
 const HomepageContext = createContext();
-const proxy = "https://gourav-node-server.herokuapp.com"; //http://localhost:8080
 
 export const HomepageData = ({ children }) => {
   const [users, setUsers] = useState([]);
@@ -12,13 +11,13 @@ export const HomepageData = ({ children }) => {
   }, []);
 
   const FetchAllData = async () => {
-    const response = await fetch(`${proxy}/users`);
+    const response = await fetch(`/users`);
     const data = await response.json();
     setUsers(data);
   };
 
   const FetchUserData = async (_id) => {
-    const response = await fetch(`${proxy}/user/${_id}`);
+    const response = await fetch(`/user/${_id}`);
     const data = await response.json();
     setUserData(data);
   };
